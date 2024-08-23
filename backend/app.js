eda4ac3be837c9fc65b40c90cca3fc97d64ddfa4
Pramit Thapa <69735326+pramit41@users.noreply.github.com>
2024-08-23 12:17:26 -0400

    sets up home component

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const morgan = require('morgan')
 
 const app = express();
+
+const PORT = process.env.PORT || 3000
+
+app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000')
+app.listen(PORT, () => {
+    console.log(`Server running on port ${3000}`)
 })
