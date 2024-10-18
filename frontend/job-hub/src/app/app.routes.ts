1642aea91ff6dd170158ae93f691e5e9a72a9234
Pramit Thapa <69735326+pramit41@users.noreply.github.com>
2024-10-18 15:47:13 -0400

    adds child route and ui changes

--- frontend/job-hub/src/app/app.routes.ts
+++ frontend/job-hub/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { AppTrackerComponent } from './components/app-tracker/app-tracker.component';
+import { AppTrackerComponent } from './components/home/app-tracker/app-tracker.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent},
-    { path: 'app-tracker', component: AppTrackerComponent},
+    { path: '', component: HomeComponent, children: [
+        { path: 'app-tracker', component: AppTrackerComponent},
+    ]},
     { path: '**', redirectTo: '', pathMatch: 'full'}
 ];
--- frontend/job-hub/src/app/components/home/home.component.ts
+++ frontend/job-hub/src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
